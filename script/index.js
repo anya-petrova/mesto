e763ecbe0ee8f4e1987736c662d2d20261682425
Anna Petrova <ann.petrova42@gmail.com>
2020-11-29 15:19:47 +0700

    change third review

--- script/index.js
+++ script/index.js
@@ -1,20 +1,21 @@
 let editButton = document.querySelector('.profile__edit-button');
 
-let popupOn = document.querySelector('.popup');
-let popupContainer = popupOn.querySelector('.popup__container');
+let popup = document.querySelector('.popup');
+let popupContainer = popup.querySelector('.popup__container');
 let closeButton = popupContainer.querySelector('.popup__close-button');
 
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
+
 let formInputName = document.querySelector('.popup__item_name');
 let formInputJob = document.querySelector('.popup__item_job');
-let formButton = document.querySelector('.popup__save-button');
+let formSaveButton = document.querySelector('.popup__save-button');
 
 function togglePopup(event) {
-    popupOn.classList.toggle('popup_opened');
+    popup.classList.toggle('popup_opened');
 }
 
-function fillForm(event) {
+function fillForm() {
     formInputName.value = profileName.textContent;
     formInputJob.value = profileJob.textContent;
 }
@@ -28,7 +29,7 @@ function formChange(event) {
 
 editButton.addEventListener('click', togglePopup);
 closeButton.addEventListener('click', togglePopup);
-formButton.addEventListener('click', formChange);
+popup.addEventListener('submit', formChange);
 editButton.addEventListener('click', fillForm);
 
 
