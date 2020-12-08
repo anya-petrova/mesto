d23808de887e1a4ee59d7ba80dd07ee71321080f
Anna Petrova <ann.petrova42@gmail.com>
2020-12-08 22:06:19 +0700

    feat: two popups fill info

--- script/index.js
+++ script/index.js
@@ -1,39 +1,114 @@
-let editButton = document.querySelector('.profile__edit-button');
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
 
-let popup = document.querySelector('.popup');
-let popupContainer = popup.querySelector('.popup__container');
-let closeButton = popupContainer.querySelector('.popup__close-button');
+let popupEdit = document.querySelector('.popup__edit');
+let popupAdd = document.querySelector('.popup__add');
+let popupContainer = document.querySelectorAll('.popup__container');
+let closeButtonEdit = document.querySelector('.popup__close-button-edit');
+let closeButtonAdd = document.querySelector('.popup__close-button-add');
 
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
+
+const formInputPlace = document.querySelector('.popup__item_place');
+const formInputLink = document.querySelector('.popup__item_link');
 
 let formInputName = document.querySelector('.popup__item_name');
 let formInputJob = document.querySelector('.popup__item_job');
 let formSaveButton = document.querySelector('.popup__save-button');
 
-function togglePopup(event) {
-    popup.classList.toggle('popup_opened');
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const listContainerElement = document.querySelector('.element__list');
+const templateElement = document.querySelector('.template');
+
+function renderList() {
+    const listItems = initialCards.map(composeItems);
+    listContainerElement.append(...listItems);
 }
 
-function fillForm() {
+renderList();
+
+function composeItems(item) {
+    const newItem = templateElement.content.cloneNode(true);
+    const newItemImage = newItem.querySelector('.element__image');
+    newItemImage.src = item.link;
+    const newItemDeleteButton = newItem.querySelector('.element__delete-button');
+    newItemDeleteButton.addEventListener('click', function(evt) {
+        const eventTarget = evt.target;
+    })
+    const newItemParagraph = newItem.querySelector('.element__paragraph');
+    newItemParagraph.textContent = item.name;
+    const newItemLikeButton = newItem.querySelector('.element__like-button');
+    newItemLikeButton.addEventListener('click', function(evt) {
+        const eventTarget = evt.target;
+        eventTarget.classList.toggle('element__like-button_active');
+    })
+    return newItem;
+
+}
+
+function toggleEditForm(evt) {
+    popupEdit.classList.toggle('popup_opened');
+}
+
+function toggleAddForm(evt) {
+    popupAdd.classList.toggle('popup_opened');
+}
+
+function fillEditForm() {
     formInputName.value = profileName.textContent;
     formInputJob.value = profileJob.textContent;
 }
 
-function formChange(event) {
+function formEditChange(event) {
     event.preventDefault();
     profileName.textContent = formInputName.value;
     profileJob.textContent = formInputJob.value;
-    togglePopup();
+    toggleEditForm();
 }
 
-editButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
-popup.addEventListener('submit', formChange);
-editButton.addEventListener('click', fillForm);
+function addNewItem(evt) {
+    evt.preventDefault();
+    const newItemFromAdd = composeItems({name:formInputPlace.value, link:formInputLink.value});
+    listContainerElement.prepend(newItemFromAdd);
+    toggleAddForm();
+}
+
+editButton.addEventListener('click', toggleEditForm);
+closeButtonEdit.addEventListener('click', toggleEditForm);
+editButton.addEventListener('click', fillEditForm);
+popupEdit.addEventListener('submit', formEditChange);
 
 
-
+addButton.addEventListener('click', toggleAddForm);
+closeButtonAdd.addEventListener('click', toggleAddForm);
+popupAdd.addEventListener('submit', addNewItem);
 
 
 
